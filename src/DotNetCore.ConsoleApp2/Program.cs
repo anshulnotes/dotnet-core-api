0b929fc0c6d3b42615475fffce7a08eca5b64be2
learning2dev <kumar.anshul@outlook.com>
2017-12-31 07:01:16 -0600

    added sample app

---
+++ src/DotNetCore.ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DotNetCore.ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
